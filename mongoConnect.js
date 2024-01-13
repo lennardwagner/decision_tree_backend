ad46e9f091b37ab78ae842f7be8aa836bb0a5eb0
lennardwagner <77745110+lennardwagner@users.noreply.github.com>
2024-01-13 15:52:17 +0100

    Added mongoDB connection

---
+++ mongoConnect.js
@@ -0,0 +1,20 @@
+const {MongoClient, ObjectId} = require("mongodb");
+
+const url = 'mongodb://localhost:27017/testDB';
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        //await client.connect();
+        const database = client.db('testDB');
+        const content = database.collection('decisionTree');
+
+        const query = {_id: new ObjectId('65a2a0cecd4ec77d85b61b48')};
+        const result = await content.findOne(query);
+
+        console.log(JSON.stringify(result, null, 2));
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
